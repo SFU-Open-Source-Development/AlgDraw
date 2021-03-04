b17f6c182ac0533d9ab922041a8c99b88bc09614
Kimia <kimianafar@yahoo.com>
2021-03-03 17:54:50 -0800

    Merge Sort comments updated

--- project/mergeSort.py
+++ project/mergeSort.py
@@ -1,51 +1,51 @@
-def merge_sort(array, left_index, right_index):
-    if left_index >= right_index:
+# merge sort is a divide and conquer sorting algorithm 
+# it recursively divides the array in half and calls the merge function on it
+
+def MergeSort(array, start, end):
+    # start and end refer to the indexes that mark the beginning and end of the array that we are trying to sort
+    if start >= end:
         return
+    
+    midpoint = (start + end)//2 
+    MergeSort (array, start, midpoint)
+    MergeSort (array, midpoint+1, end)
+    merge (array, start, end, midpoint)
 
-    middle = (left_index + right_index)//2
-    merge_sort(array, left_index, middle)
-    merge_sort(array, middle + 1, right_index)
-    merge(array, left_index, right_index, middle)
 
+# merge function makes copies of the two smaller arrays given to the funciton
+# copies the elements in a sorted manner back into the original array
+def merge (array, start, end, midpoint):
+    # first, we need to make copies of the two sub arrays
+    left_array = array[start:midpoint+1]
+    right_array = array[midpoint+1:end+1]
 
-def merge(array, left_index, right_index, middle):
-    # Make copies of both arrays we're trying to merge
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = start
+    
+    while left_array_index < len(left_array) and right_array_index < len(right_array):
+        # goes through elements of both subarray one by one
+        # copies the smaller element between into the sorted array
 
-    # The second parameter is non-inclusive, so we have to increase by 1
-    left_copy = array[left_index:middle + 1]
-    right_copy = array[middle+1:right_index+1]
-
-    # Initial values for variables that we use to keep
-    # track of where we are in each array
-    left_copy_index = 0
-    right_copy_index = 0
-    sorted_index = left_index
-
-    # Go through both copies until we run out of elements in one
-    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
-
-        # If our left_copy has the smaller element, put it in the sorted
-        # part and then move forward in left_copy (by increasing the pointer)
-        if left_copy[left_copy_index] <= right_copy[right_copy_index]:
-            array[sorted_index] = left_copy[left_copy_index]
-            left_copy_index = left_copy_index + 1
-        # Opposite from above
+        if left_array[left_array_index] <= right_array[right_array_index]:
+            array[sorted_index] = left_array[left_array_index]
+            left_array_index += 1
         else:
-            array[sorted_index] = right_copy[right_copy_index]
-            right_copy_index = right_copy_index + 1
+            array[sorted_index] = right_array[right_array_index]
+            right_array_index += 1
+        
+        # regardless of subarray indexes, sorted_index should increase
+        sorted_index += 1
+    
+    # if elements of either subarary run out, copy remaining elements of other subarray in sorted array
+    # subarrays are assumed to be sorted due to recursion
 
-        # Regardless of where we got our element from
-        # move forward in the sorted part
-        sorted_index = sorted_index + 1
-
-    # We ran out of elements either in left_copy or right_copy
-    # so we will go through the remaining elements and add them
-    while left_copy_index < len(left_copy):
-        array[sorted_index] = left_copy[left_copy_index]
-        left_copy_index = left_copy_index + 1
-        sorted_index = sorted_index + 1
-
-    while right_copy_index < len(right_copy):
-        array[sorted_index] = right_copy[right_copy_index]
-        right_copy_index = right_copy_index + 1
-        sorted_index = sorted_index + 1
+    while left_array_index < len(left_array):
+        array[sorted_index] = left_array[left_array_index]
+        sorted_index += 1
+        left_array_index += 1
+    
+    while right_array_index < len(right_array):
+        array[sorted_index] = right_array[right_array_index]
+        sorted_index += 1
+        right_array_index += 1
