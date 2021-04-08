a3c105108d84cce73a5ea0c272d83d8ed5e04671
gwenyang02 <gwenyang02@gmail.com>
2021-04-07 21:07:35 -0700

    added parameters filename and length to read_file()

--- src/InputParser.py
+++ src/InputParser.py
@@ -1,13 +1,17 @@
-from typing import Any, Union
-
 import pandas as pd
 import numpy as np
+from tkinter import filedialog
+from tkinter import *
+
+# prompt user for text file
+def input_prompt():
+    root = Tk()
+    root.withdraw()  # hide window
+    promptfile = filedialog.askopenfilename(mode='r',filetypes=(("Text Files", ".txt .csv"), ("All Files", "*.*")))
+    root.destroy()
+    return promptfile
 
 # validates file name input
-from pandas import DataFrame, Series
-from pandas.io.parsers import TextFileReader
-
-
 def input_valid():
     ans_valid = False
     while not ans_valid:
@@ -21,23 +25,19 @@ def input_valid():
 
 # take in user inputs
 def input_cmd():
-
-    filename = input_valid() # takes in file name
-
     # possible for user to input column name
     # but needs if-statement to verify if user wants to input numbers or names
     col_input = input("Column numbers to be read (separated by ','): ")
     col_list = col_input.split(",")
     for i in range(len(col_list)):
         col_list[i] = int(col_list[i])
-
     #num_headers = int(input("How many headers in file?: "))
-
     file_delim = input("Type of column delimiter:")
     return filename, file_delim, col_list
 
-def read_file(filename, file_delim, col_list):
-    f: Union[Union[TextFileReader, Series, DataFrame, None], Any] = pd.read_csv(filename, encoding='utf-8',
+
+def read_textfile(filename, file_delim, col_list):
+    f= pd.read_csv(filename, encoding='utf-8',
                    sep=file_delim, usecols=col_list)
     # https://www.kite.com/python/docs/pandas.DataFrame.replace
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.dropna.html
@@ -45,6 +45,14 @@ def read_file(filename, file_delim, col_list):
     for i in col_list:
         col_list.append(float(i))  # convert each object in columns to float
     return filename, col_list  # col_list are the list of numbers to be sorted
+
+def read_excelfile(filename, file_delim, col_list):
+    f = pd.read_excel(filename, encoding='utf-8',
+                          sep=file_delim, usecols=col_list)
+    f.replace(r'^\s*$', np.nan, inplace=True)
+    for i in col_list:
+        col_list.append(float(i))  # convert each object in columns to float
+    return filename, col_list
 
     # if f.mode == 'r':
     #     f1 = f.readlines()
@@ -59,7 +67,6 @@ def read_file(filename, file_delim, col_list):
 if __name__ == "__main__":
 
     filename, file_delim, col_list = input_cmd()
-    read_file(filename, file_delim, col_list) #
 
-    # read_file(r"C:\Users\Aliyah\OSDC\greetings.txt", 3)
-    # print("thanks conor :D")
+    read_textfile(filename, file_delim, col_list) #
+
